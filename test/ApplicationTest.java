00c7de93b6d16918c6348a8fd6bc4cbee6c5adea
kylehebert <kyle.hebert@gmail.com>
2016-05-04 12:13:47 -0500

    Integration tests for adding and deleting volunteers and locations passing

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,23 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-import play.twirl.api.Content;
-
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
 
 
 /**
@@ -27,19 +7,6 @@ import static org.junit.Assert.*;
  *
  */
 public class ApplicationTest {
-
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertEquals(2, a);
-    }
-
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertEquals("text/html", html.contentType());
-        assertTrue(html.body().contains("Your new application is ready."));
-    }
 
 
 }
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,13 +1,7 @@
 import org.junit.*;
 
-import play.mvc.*;
-import play.test.*;
-
 import static play.test.Helpers.*;
 import static org.junit.Assert.*;
-
-
-import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
 
@@ -37,4 +31,83 @@ public class IntegrationTest {
         });
     }
 
+    @Test
+    public void deleteVolunteerTest() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333/volunteers/new");
+
+            // add a volunteer
+            browser.fill("input[name='firstName']").with("first");
+            browser.fill("input[name='lastName']").with("last");
+            browser.fill("input[name='address1']").with("123 Main Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // delete the volunteer by clicking the trash button
+            browser.find(".glyphicon-trash").click();
+
+            // make sure the "no volunteers" help block is present
+            assertTrue(browser.find((".help-block")).getText().contains("There are no volunteers in the database."));
+
+        });
+
+
+    }
+
+    @Test
+    public void addLocationTest() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333/locations/new");
+
+            // add a location
+            browser.fill("input[name='name']").with("test location");
+            browser.fill("input[name='address1']").with("123 Test Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // go to the location detail page
+            browser.$("table td a").first().click();
+
+            // assert "test location" is present
+            assertTrue(browser.$("legend").getText().contains("test location"));
+
+        });
+    }
+
+    @Test
+    public void deleteLocationTest() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333/locations/new");
+
+            // add a location
+            browser.fill("input[name='name']").with("test location");
+            browser.fill("input[name='address1']").with("123 Test Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // delete the location by clicking the trash button
+            browser.find(".glyphicon-trash").click();
+
+            // make sure the "no locations" help block is present
+            assertTrue(browser.find((".help-block")).getText().contains("There are no locations in the database."));
+
+
+        });
+
+    }
 }
