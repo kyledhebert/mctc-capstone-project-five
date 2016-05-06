aeebf2d590888f583066e4f08ff47dd5ed242803
kylehebert <kyle.hebert@gmail.com>
2016-05-06 18:25:03 -0500

    tests for adding and deleting locations from assignments, all six tests passing

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,5 +1,7 @@
 import org.junit.*;
+import org.openqa.selenium.By;
 
+import static org.fluentlenium.core.filter.FilterConstructor.withText;
 import static play.test.Helpers.*;
 import static org.junit.Assert.*;
 
@@ -104,10 +106,97 @@ public class IntegrationTest {
             browser.find(".glyphicon-trash").click();
 
             // make sure the "no locations" help block is present
-            assertTrue(browser.find((".help-block")).getText().contains("There are no locations in the database."));
+            assertTrue(browser.find((".help-block")).getText().contains("There must be at least one location"));
 
 
         });
+
+    }
+
+    @Test
+    public void addAssignmentToLocationTest() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333/locations/new");
+
+            // add a location
+            browser.fill("input[name='name']").with("test location");
+            browser.fill("input[name='address1']").with("123 Test Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the location table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // go to the location detail page
+            browser.$("table td a").first().click();
+            assertTrue(browser.$("legend").getText().contains("test location"));
+
+            // make sure the assignment table is empty
+            assertTrue(browser.$("table td a").isEmpty());
+
+            // click the new assignment link
+            browser.find(".add-link").click();
+            assertTrue(browser.$("legend").getText().contains("Assignment (New)"));
+
+            // add a new assignment
+            browser.fill("input[name='assignmentName']").with("test assignment");
+            browser.submit("button[type='submit']");
+
+            // go to the assignment detail page
+            browser.find(".glyphicon-pencil").click();
+
+            // assert "test assignment" is present
+            assertTrue(browser.$("legend").getText().contains("test assignment"));
+
+        });
+    }
+
+    @Test
+    public void removeAssignmentFromLocationTest() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333/locations/new");
+
+            // add a location
+            browser.fill("input[name='name']").with("test location");
+            browser.fill("input[name='address1']").with("123 Test Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the location table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // go to the location detail page
+            browser.$("table td a").first().click();
+            assertTrue(browser.$("legend").getText().contains("test location"));
+
+            // make sure the assignment table is empty
+            assertTrue(browser.$("table td a").isEmpty());
+
+            // click the new assignment link
+            browser.find(".add-link").click();
+            assertTrue(browser.$("legend").getText().contains("Assignment (New)"));
+
+            // add a new assignment
+            browser.fill("input[name='assignmentName']").with("test assignment");
+            browser.submit("button[type='submit']");
+
+            // make sure the assignment is in the assignment table
+            assertTrue(browser.$("table td").first().getText().contains("test assignment"));
+
+            // delete the assignment
+            browser.find(".glyphicon-trash").click();
+
+            // make sure the "no assignments" text is visible
+            assertTrue(browser.find(("p")).getText().contains("There are no assignments for this location"));
+
+
+
+        });
+
 
     }
 }
