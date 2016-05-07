3e0e16134560591359febf989869db212d617d96
kylehebert <kyle.hebert@gmail.com>
2016-05-07 11:12:51 -0500

    tests for adding and removing volunteers from assignments, all 8 tests are passing

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,9 +1,8 @@
-import org.junit.*;
-import org.openqa.selenium.By;
+import org.junit.Test;
 
-import static org.fluentlenium.core.filter.FilterConstructor.withText;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static play.test.Helpers.*;
-import static org.junit.Assert.*;
 
 public class IntegrationTest {
 
@@ -108,7 +107,6 @@ public class IntegrationTest {
             // make sure the "no locations" help block is present
             assertTrue(browser.find((".help-block")).getText().contains("There must be at least one location"));
 
-
         });
 
     }
@@ -193,7 +191,151 @@ public class IntegrationTest {
             // make sure the "no assignments" text is visible
             assertTrue(browser.find(("p")).getText().contains("There are no assignments for this location"));
 
+        });
 
+    }
+
+    @Test
+    public void addVolunteertoAssignment() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+
+            browser.goTo("http://localhost:3333/volunteers/new");
+
+            // add a volunteer
+            browser.fill("input[name='firstName']").with("first");
+            browser.fill("input[name='lastName']").with("last");
+            browser.fill("input[name='address1']").with("123 Main Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // go to the volunteer detail page
+            browser.$("table td a").first().click();
+
+            // assert "test volunteer" is present
+            assertTrue(browser.$("legend").getText().contains("first last"));
+
+
+            browser.goTo("http://localhost:3333/locations/new");
+
+            // add a location
+            browser.fill("input[name='name']").with("test location");
+            browser.fill("input[name='address1']").with("123 Test Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the location table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // go to the location detail page
+            browser.$("table td a").first().click();
+            assertTrue(browser.$("legend").getText().contains("test location"));
+
+            // make sure the assignment table is empty
+            assertTrue(browser.$("table td a").isEmpty());
+
+            // click the new assignment link
+            browser.find(".add-link").click();
+            assertTrue(browser.$("legend").getText().contains("Assignment (New)"));
+
+            // add a new assignment
+            browser.fill("input[name='assignmentName']").with("test assignment");
+            browser.submit("button[type='submit']");
+
+            // go to the assignment detail page
+            browser.find(".glyphicon-pencil").click();
+
+            // assert "test assignment" is present
+            assertTrue(browser.$("legend").getText().contains("test assignment"));
+
+            // add the test volunteer to the assignment
+            browser.find(".add-link").click();
+            browser.find(".glyphicon-plus").click();
+
+            // verify "first last" is in assigned volunteers table
+            assertTrue(browser.$("table td a").getText().contains("first"));
+
+        });
+
+    }
+
+    @Test
+    public void removeVolunteerFromAssignment() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+
+            browser.goTo("http://localhost:3333/volunteers/new");
+
+            // add a volunteer
+            browser.fill("input[name='firstName']").with("first");
+            browser.fill("input[name='lastName']").with("last");
+            browser.fill("input[name='address1']").with("123 Main Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // go to the volunteer detail page
+            browser.$("table td a").first().click();
+
+            // assert "test volunteer" is present
+            assertTrue(browser.$("legend").getText().contains("first last"));
+
+
+            browser.goTo("http://localhost:3333/locations/new");
+
+            // add a location
+            browser.fill("input[name='name']").with("test location");
+            browser.fill("input[name='address1']").with("123 Test Street");
+            browser.fill("input[name='city']").with("Testville");
+            browser.fill("input[name='state']").with("MN");
+            browser.fill("input[name='zipCode']").with("55116");
+            browser.submit("button[type='submit']");
+
+            // make sure the location table is not empty
+            assertFalse(browser.$("table td a").isEmpty());
+
+            // go to the location detail page
+            browser.$("table td a").first().click();
+            assertTrue(browser.$("legend").getText().contains("test location"));
+
+            // make sure the assignment table is empty
+            assertTrue(browser.$("table td a").isEmpty());
+
+            // click the new assignment link
+            browser.find(".add-link").click();
+            assertTrue(browser.$("legend").getText().contains("Assignment (New)"));
+
+            // add a new assignment
+            browser.fill("input[name='assignmentName']").with("test assignment");
+            browser.submit("button[type='submit']");
+
+            // go to the assignment detail page
+            browser.find(".glyphicon-pencil").click();
+
+            // assert "test assignment" is present
+            assertTrue(browser.$("legend").getText().contains("test assignment"));
+
+            // add the test volunteer to the assignment
+            browser.find(".add-link").click();
+            browser.find(".glyphicon-plus").click();
+
+            // verify "first last" is in assigned volunteers table
+            assertTrue(browser.$("table td a").getText().contains("first"));
+
+            // remove the volunteer
+            browser.find(".glyphicon-remove").click();
+
+            // verify the the "add volunteers" help block is visible
+            assertTrue(browser.find((".help-block")).getText().contains("Add volunteers from the list below."));
 
         });
 
