eac537d6ae34af12ca00ae5d46e12931efc8ad1b
kylehebert <kyle.hebert@gmail.com>
2016-05-07 11:41:38 -0500

    added model tests for Volunteer, Assignment and Location, all three tests passing

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,4 +1,9 @@
+import models.Assignment;
+import models.Location;
+import models.Volunteer;
+import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -8,5 +13,45 @@
  */
 public class ApplicationTest {
 
+    @Test
+    public void createVolunteerTest() {
+
+        Volunteer.VolunteerBuilder volunteerBuilder = new Volunteer.VolunteerBuilder("first",
+                "last", "Active", "123 Test Street", "Testville", "MN", "55116");
+
+        Volunteer volunteer = new Volunteer(volunteerBuilder);
+
+        assertTrue(volunteer.getFirstName().equals("first"));
+        assertTrue(volunteer.getLastName().equals("last"));
+        assertTrue(volunteer.getStatus().equals("Active"));
+        assertTrue(volunteer.getAddress1().equals("123 Test Street"));
+        assertTrue(volunteer.getCity().equals("Testville"));
+        assertTrue(volunteer.getState().equals("MN"));
+        assertTrue(volunteer.getZipCode().equals("55116"));
+    }
+
+    @Test
+    public void createLocationTest() {
+
+        Location.LocationBuilder locationBuilder = new Location.LocationBuilder("test location",
+                "123 test street", "testville", "MN", "55116");
+
+        Location location = new Location(locationBuilder);
+
+        assertTrue(location.getName().equals("test location"));
+        assertTrue(location.getAddress1().equals("123 test street"));
+        assertTrue(location.getCity().equals("testville"));
+        assertTrue(location.getState().equals("MN"));
+        assertTrue(location.getZipCode().equals("55116"));
+    }
+
+    @Test
+    public void createAssignmentTest() {
+        Assignment assignment = new Assignment();
+
+        assignment.setAssignmentName("test assignment");
+
+        assertTrue(assignment.getAssignmentName().equals("test assignment"));
+    }
 
 }
