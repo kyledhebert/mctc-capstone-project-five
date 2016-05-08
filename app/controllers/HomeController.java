581867f03b35bc048c36283b8e60b6c90a5e8b6a
kylehebert <kyle.hebert@gmail.com>
2016-05-08 14:52:26 -0500

    added statistics to home page

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import com.google.inject.Inject;
+import org.hibernate.Session;
+import play.db.jpa.JPAApi;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+
+import javax.persistence.EntityManager;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -10,14 +16,58 @@ import views.html.index;
  */
 public class HomeController extends Controller {
 
+    private JPAApi jpaApi;
+
+    @Inject
+    public HomeController(JPAApi jpaApi) {
+        this.jpaApi = jpaApi;
+    }
+
+    // returns a Hibernate session for database transactions
+    private Session getSession() {
+        EntityManager entityManager  = jpaApi.em();
+        return (Session) entityManager.getDelegate();
+    }
+
+    private Integer getVolunteerCount() {
+
+        Session session = getSession();
+
+        return ((Number) session.createQuery("select count(*) from Volunteer ").iterate().next()).intValue();
+    }
+
+    private Integer getLocationCount() {
+
+        Session session = getSession();
+
+        return ((Number) session.createQuery("select count(*) from Location ").iterate().next()).intValue();
+    }
+
+    private Integer getAssignmentCount() {
+
+        Session session = getSession();
+
+        return ((Number) session.createQuery("select count(*) from Assignment ").iterate().next()).intValue();
+    }
+
+
+
     /**
      * An action that renders the Home page of the application.
      * The configuration in the <code>routes</code> file means that
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
+    @Transactional
     public Result index() {
-        return ok(index.render());
+
+
+        int volunteerCount = getVolunteerCount();
+        int locationCount = getLocationCount();
+        int assignmentCount = getAssignmentCount();
+
+        return ok(index.render(volunteerCount, locationCount, assignmentCount));
     }
+
 
 }
